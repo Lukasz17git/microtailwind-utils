a14488f53e06d912768d1f94a5bd7060d80bee08
Lukasz17git <lucas.m2295@gmail.com>
2023-11-25 01:07:21 +0100

    added main test

---
+++ .changeset/kind-kids-move.md
@@ -0,0 +1,5 @@
+---
+"microtailwind-utils": patch
+---
+
+added main test
---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { it, describe, expect } from 'vitest'
+
+describe("main test", () => {
+   it('should return true', () => {
+      expect(true).toBe(true)
+   })
+})
